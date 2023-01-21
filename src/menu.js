912c4101f791b73bbca42113640dd944fe4f6cad
FunChosa <gilazieva.00@mail.ru>
2023-01-21 12:35:49 +0300

    menu.js and app.js were updated

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,20 @@
-import {Menu} from './core/menu'
+import {Menu} from './core/menu';
 
 export class ContextMenu extends Menu {
-
+    constructor(selector) {
+        super(selector);
+        this.moduleList = [];
+      }
+      
+    open() {
+        this.el.classList.add('open');
+    }
+    close() {
+        this.el.classList.remove('open');
+    }
+    add(module, options) {
+        const contextModule = new module(options.type, options.text);
+        this.el.innerHTML += contextModule.toHTML();
+        this.moduleList.push(contextModule);
+    }
 }

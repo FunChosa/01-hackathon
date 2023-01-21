2778d9bff360f3716baf9a1501525a619c6e279b
FunChosa <gilazieva.00@mail.ru>
2023-01-21 21:37:02 +0300

    emoji-module created

--- src/modules/emoji.module.js
+++ src/modules/emoji.module.js
@@ -1,5 +1,41 @@
 import {Module} from '../core/module'
+import {random} from '../utils';
 
 export class EmojiModule extends Module {
+    constructor(type, text){
+        super(type, text);
+        this.type = type;
+        this.text = text;
+    }
+    trigger() {
+  
+        const body = document.querySelector('body');
+       
+        const curretnEmoji = document.querySelector('.emoji');
+        if (curretnEmoji){
+            curretnEmoji.remove();
+            createEmoji();
+        } else {
+            createEmoji();
+        }
 
+        function createEmoji(){
+            const emojiBlock = document.createElement('div');
+            emojiBlock.classList.add('emoji');
+            emojiBlock.style.fontSize = '300px';
+            emojiBlock.style.marginTop = '10vh';
+
+            emojiBlock.style.textAlign = 'center';
+            emojiBlock.innerHTML = `&#${random(128521, 128591)}`
+            body.prepend(emojiBlock);
+
+            setTimeout(() => {
+                emojiBlock.remove();
+            }, 5000);
+        }
+    }
+     
+    toHTML() {
+       return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
+    }
 }
